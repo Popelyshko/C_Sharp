f41c11ae0394af901bfe987fa6f2e39d14b1569d
Popelyshko <49385639+Popelyshko@users.noreply.github.com>
2023-07-04 09:39:29 +0300

    Добавила 4 задачи первого урока

---
+++ Урок_1/Задача_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите два числа");
+
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+
+int max = Math.Max(a, b);
+int min = Math.Min(a, b);
+
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");
+
+
---
+++ Урок_1/Задача_4/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите три числа");
+
+Console.Write("a = ");
+        string inputA = Console.ReadLine();
+        int a;
+        while (!int.TryParse(inputA, out a))
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число:");
+            Console.Write("a = ");
+            inputA = Console.ReadLine();
+        }
+
+        Console.Write("b = ");
+        string inputB = Console.ReadLine();
+        int b;
+        while (!int.TryParse(inputB, out b))
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число:");
+            Console.Write("b = ");
+            inputB = Console.ReadLine();
+        }
+
+        Console.Write("c = ");
+        string inputC = Console.ReadLine();
+        int c;
+        while (!int.TryParse(inputC, out c))
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число:");
+            Console.Write("c = ");
+            inputC = Console.ReadLine();
+        }
+
+int max = Math.Max(a, Math.Max(b, c));
+
+Console.WriteLine($"Максимальное число: {max}");
+
